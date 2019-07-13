ae3b3ab317ab3a05a619e43f2ead6c6eed1b0d1f
Abhiraj Singh <abhirajsinghofficial@gmail.com>
2019-07-13 10:49:41 +0530

    first push

---
+++ app/src/main/java/com/oxoo/spagreen/Config.java
@@ -0,0 +1,14 @@
+package com.oxoo.spagreen;
+
+public class Config {
+
+    // copy your api url from php admin dashboard & paste below
+    public static final String API_SERVER_URL = "http://techturnz.com/jet/api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "wp4ucf6m2dv1uq7586562i2b";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "https://www.google.com:";
+
+}
